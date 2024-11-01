84c3ded8d4185a603473dc848de9cc511788738d
thaemelsks <162651090+thaemelsks@users.noreply.github.com>
2024-10-31 22:36:09 -0300

    Add files via upload

---
+++ desenvolvendo ferramentas/assets/perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+     'lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'tchau em ingles se diz bye (bai)')
+
+
+    criaCartao(
+        'lingua inglesa',
+        'Como se diz o que em ingles?',
+       'o que em ingles se diz what '
+       )
+
+       criaCartao(
+        'Geografia',
+        'Qual a capital da Italia?',
+        'A capital da Italia é Roma'
+    )
+   
+    criaCartao(
+        'Geografia',
+        'Qual a capital do brasil?',
+        'A capital do Brasil é Brasilia'
+    )
+
+    criaCartao(
+        'matematica',
+        'quanto e 50% de 90?',
+        '50% de 90 e 45'
+    )
+
+    criaCartao(
+        'matematica',
+        'qual a raiz quadrada de 81?',
+        'a raiz quadrada de 81 e 9'
+    )
+
